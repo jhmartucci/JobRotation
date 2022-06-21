47586625c6929e0ee4fb9bd4095319fad5b98bcd
jhmartucci <97127958+jhmartucci@users.noreply.github.com>
2022-06-21 13:55:02 -0300

    Envio exercicio sequencia Fibonacci

---
+++ Fibonacci/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import Models.Fibonacci;
+
+public class Main {
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci();
+
+        fibonacci.calculaFibonacci();
+        fibonacci.MostraSequenciaFibonacci();
+        fibonacci.verificaNumeroNaSequencia();
+
+    }
+}
---
+++ Fibonacci/src/main/java/Models/Fibonacci.java
@@ -0,0 +1,40 @@
+package Models;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Fibonacci {
+    int inicio = 0;
+    int countador = 1;
+    int aux = 0;
+    List<Integer> fibonacci = new ArrayList<>();
+
+    public List<Integer> calculaFibonacci(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Quantos números devemos considerar a seguencia de Fibonacci...: ");
+        int tamanho = scanner.nextInt();
+         for(int i = 0; i < tamanho; i++){
+             fibonacci.add(inicio);
+             aux = inicio;
+             inicio += countador;
+             countador = aux;
+         }
+         return fibonacci;
+    }
+    public void MostraSequenciaFibonacci(){
+        System.out.println(fibonacci);
+    }
+    public void verificaNumeroNaSequencia(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Qual número devemos verificar se pertence a sequencia de Fibonacci...: ");
+        int num = scanner.nextInt();
+
+        if(fibonacci.contains(num)){
+            System.out.println("O número " + num + " pertence a sequencia de Fibonacci");
+        }else{
+            System.out.println("O número " + num + " não pertence a sequencia de Fibonacci");
+        }
+    }
+
+}
